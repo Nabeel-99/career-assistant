9bc99e03bbc7b4b1fadbf01c4e6da94c957d9a0e
Farouk Nabeel <farouknabil810@gmail.com>
2025-07-02 22:02:34 +0100

    wrap reset-password around suspense

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,18 +1,24 @@
 "use client";
-
 import ResetPasswordForm from "@/components/forms/ResetPasswordForm";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-const page = () => {
+const TokenWrapper = () => {
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
-  //   console.log("token", token);
+
+  return <ResetPasswordForm token={token!} />;
+};
+
+const Page = () => {
   return (
-    <div className="flex flex-col relative bg-gradient-to-tr from-[#0a0a0a] from-70% to-transparent   items-center justify-center w-full h-screen">
-      <div className="absolute w-full  h-full bg-hero"></div>
-      <ResetPasswordForm token={token!} />
+    <div className="flex flex-col relative bg-gradient-to-tr from-[#0a0a0a] from-70% to-transparent items-center justify-center w-full h-screen">
+      <div className="absolute w-full h-full bg-hero"></div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <TokenWrapper />
+      </Suspense>
     </div>
   );
 };
 
-export default page;
+export default Page;
